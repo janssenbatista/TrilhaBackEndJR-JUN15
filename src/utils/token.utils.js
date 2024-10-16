2c835b624f962aebd95d782d0d9f1e6dabfc121b
Janssen Batista <batistajanssen.dev@gmail.com>
2024-10-16 10:36:52 -0300

    feat: add function to generate and validate jwt

---
+++ src/utils/token.utils.js
@@ -0,0 +1,20 @@
+import { UNAUTHORIZED } from "../constants/httpStatusCode.js";
+
+export const generateToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+};
+
+export const verifyToken = (token, res) => {
+  try {
+    jwt.verify(token, process.env.JWT_SECRET, (error, data) => {
+      if (error) {
+        return res.status(UNAUTHORIZED).json({ message: "Invalid token" });
+      }
+      res.userId = data.userId;
+    });
+  } catch (error) {
+    return res.status(UNAUTHORIZED).json({ message: "Invalid token" });
+  }
+};
